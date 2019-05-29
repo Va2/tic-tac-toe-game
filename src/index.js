0d27bfe49ff16e3bc4f4c003335be7962b93578e
Va2 <vadim.brieven@gmail.com>
2019-05-29 10:24:33 +0200

    Adding time travel: Showing the past moves

--- src/index.js
+++ src/index.js
@@ -77,6 +77,18 @@ class Game extends React.Component {
         const history = this.state.history;
         const current = history[history.length - 1];
         const winner = calculateWinner(current.squares);
+
+        const moves = history.map((step, move) => {
+            const desc = move ?
+                'Revenir au tour n°' + move
+                : 'Revenir au début de la partie';
+            return (
+                <li>
+                    <button onClick={() => this.jumpTo(move)}>{desc}</button>
+                </li>
+            )
+        })
+
         let status;
 
         if (winner) {
@@ -95,7 +107,7 @@ class Game extends React.Component {
                 </div>
                 <div className="game-info">
                     <div>{status}</div>
-                    <ol>{/* TODO */}</ol>
+                    <ol>{moves}</ol>
                 </div>
             </div>
         );
